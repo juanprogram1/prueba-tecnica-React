c8fa1a2eaef3811fa7aeb9606a1f826658385827
Juan David Carrillo <anilloj62@gmail.com>
2025-09-22 13:19:34 -0500

    Se hizo el visualizador grafico

---
+++ cryptoinvestment/Frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./css/App.css";
+import { ChartComponent } from "./components/ChartView";
+
+function App() {
+  return (
+    <>
+      <main className="pt-10">
+        <section>
+          <ChartComponent />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ cryptoinvestment/Frontend/src/Type.ts
@@ -0,0 +1,10 @@
+export interface ChartComponentProps {
+  data?: { time: string; value: number }[];
+  colors?: {
+    backgroundColor?: string;
+    lineColor?: string;
+    textColor?: string;
+    areaTopColor?: string;
+    areaBottomColor?: string;
+  };
+}
